cc4f79d1c284f03c7018e0f527923c6f812774bc
earpboy1064 <31712696+earpboy1064@users.noreply.github.com>
2023-05-02 02:11:37 -0500

    commit

---
+++ src/main/java/servlets/ReserveNavServlet.java
@@ -0,0 +1,51 @@
+package servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+@WebServlet(name = "ReserveNavServlet", value = "/ReserveNavServlet")
+public class ReserveNavServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        HttpSession session = request.getSession();
+
+        boolean loggedIn = false;
+        try {
+            loggedIn = (boolean) session.getAttribute("loggedIn");
+        }
+        catch(Exception e)
+        {
+
+        }
+        if (loggedIn == true) {
+
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("reserve.jsp");
+            request.setAttribute("login", true);
+            requestDispatcher.forward(request, response);
+        }
+        else {
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("Login.jsp");
+            session.setAttribute("file","reserve.jsp");
+            request.setAttribute("error", "Passwords must match!");
+            requestDispatcher.forward(request, response);
+        }
+
+    }
+}
+
+
